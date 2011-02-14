515151387056f581e87c3b5c6c29b99ce788d3dd
Eliezer Talon <elitalon@inventiaplus.com>
2011-02-14 08:27:43 +0000

    Temporary module sample created with default actions

---
+++ apps/frontend/modules/sample/actions/actions.class.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * sample actions.
+ *
+ * @package    bna_green_house
+ * @subpackage sample
+ * @author     Eliezer Talon <elitalon@inventiaplus.com>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sampleActions extends sfActions
+{
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->samples = Doctrine_Core::getTable('Sample')
+      ->createQuery('a')
+      ->execute();
+  }
+
+  public function executeShow(sfWebRequest $request)
+  {
+    $this->sample = Doctrine_Core::getTable('Sample')->find(array($request->getParameter('id')));
+    $this->forward404Unless($this->sample);
+  }
+
+  public function executeNew(sfWebRequest $request)
+  {
+    $this->form = new SampleForm();
+  }
+
+  public function executeCreate(sfWebRequest $request)
+  {
+    $this->forward404Unless($request->isMethod(sfRequest::POST));
+
+    $this->form = new SampleForm();
+
+    $this->processForm($request, $this->form);
+
+    $this->setTemplate('new');
+  }
+
+  public function executeEdit(sfWebRequest $request)
+  {
+    $this->forward404Unless($sample = Doctrine_Core::getTable('Sample')->find(array($request->getParameter('id'))), sprintf('Object sample does not exist (%s).', $request->getParameter('id')));
+    $this->form = new SampleForm($sample);
+  }
+
+  public function executeUpdate(sfWebRequest $request)
+  {
+    $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
+    $this->forward404Unless($sample = Doctrine_Core::getTable('Sample')->find(array($request->getParameter('id'))), sprintf('Object sample does not exist (%s).', $request->getParameter('id')));
+    $this->form = new SampleForm($sample);
+
+    $this->processForm($request, $this->form);
+
+    $this->setTemplate('edit');
+  }
+
+  public function executeDelete(sfWebRequest $request)
+  {
+    $request->checkCSRFProtection();
+
+    $this->forward404Unless($sample = Doctrine_Core::getTable('Sample')->find(array($request->getParameter('id'))), sprintf('Object sample does not exist (%s).', $request->getParameter('id')));
+    $sample->delete();
+
+    $this->redirect('sample/index');
+  }
+
+  protected function processForm(sfWebRequest $request, sfForm $form)
+  {
+    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
+    if ($form->isValid())
+    {
+      $sample = $form->save();
+
+      $this->redirect('sample/edit?id='.$sample->getId());
+    }
+  }
+}
---
+++ apps/frontend/modules/sample/templates/showSuccess.php
@@ -0,0 +1,90 @@
+<table>
+  <tbody>
+    <tr>
+      <th>Id:</th>
+      <td><?php echo $sample->getId() ?></td>
+    </tr>
+    <tr>
+      <th>Number:</th>
+      <td><?php echo $sample->getNumber() ?></td>
+    </tr>
+    <tr>
+      <th>Ecosystem:</th>
+      <td><?php echo $sample->getEcosystemId() ?></td>
+    </tr>
+    <tr>
+      <th>Location:</th>
+      <td><?php echo $sample->getLocation() ?></td>
+    </tr>
+    <tr>
+      <th>Latitude degrees:</th>
+      <td><?php echo $sample->getLatitudeDegrees() ?></td>
+    </tr>
+    <tr>
+      <th>Longitude degrees:</th>
+      <td><?php echo $sample->getLongitudeDegrees() ?></td>
+    </tr>
+    <tr>
+      <th>Latitude minutes:</th>
+      <td><?php echo $sample->getLatitudeMinutes() ?></td>
+    </tr>
+    <tr>
+      <th>Longitude minutes:</th>
+      <td><?php echo $sample->getLongitudeMinutes() ?></td>
+    </tr>
+    <tr>
+      <th>Environment:</th>
+      <td><?php echo $sample->getEnvironmentId() ?></td>
+    </tr>
+    <tr>
+      <th>Habitat:</th>
+      <td><?php echo $sample->getHabitatId() ?></td>
+    </tr>
+    <tr>
+      <th>Ph:</th>
+      <td><?php echo $sample->getPh() ?></td>
+    </tr>
+    <tr>
+      <th>Conductivity:</th>
+      <td><?php echo $sample->getConductivity() ?></td>
+    </tr>
+    <tr>
+      <th>Temperature:</th>
+      <td><?php echo $sample->getTemperature() ?></td>
+    </tr>
+    <tr>
+      <th>Salinity:</th>
+      <td><?php echo $sample->getSalinity() ?></td>
+    </tr>
+    <tr>
+      <th>Close picture:</th>
+      <td><?php echo $sample->getClosePicture() ?></td>
+    </tr>
+    <tr>
+      <th>Laboratory picture:</th>
+      <td><?php echo $sample->getLaboratoryPicture() ?></td>
+    </tr>
+    <tr>
+      <th>Collector:</th>
+      <td><?php echo $sample->getCollectorId() ?></td>
+    </tr>
+    <tr>
+      <th>Collection date:</th>
+      <td><?php echo $sample->getCollectionDate() ?></td>
+    </tr>
+    <tr>
+      <th>Created at:</th>
+      <td><?php echo $sample->getCreatedAt() ?></td>
+    </tr>
+    <tr>
+      <th>Updated at:</th>
+      <td><?php echo $sample->getUpdatedAt() ?></td>
+    </tr>
+  </tbody>
+</table>
+
+<hr />
+
+<a href="<?php echo url_for('sample/edit?id='.$sample->getId()) ?>">Edit</a>
+&nbsp;
+<a href="<?php echo url_for('sample/index') ?>">List</a>
