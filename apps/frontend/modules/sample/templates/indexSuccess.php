5ed9e023944f814d4e46b8006947504e38521ed0
Eliezer Talon <elitalon@inventiaplus.com>
2011-02-14 09:59:33 +0000

    Useless fields remove from index and new actions

--- apps/frontend/modules/sample/templates/indexSuccess.php
+++ apps/frontend/modules/sample/templates/indexSuccess.php
@@ -1,56 +1,30 @@
 <h1>Samples List</h1>
 
 <table>
-  <thead>
-    <tr>
-      <th>Id</th>
-      <th>Number</th>
-      <th>Ecosystem</th>
-      <th>Location</th>
-      <th>Latitude degrees</th>
-      <th>Longitude degrees</th>
-      <th>Latitude minutes</th>
-      <th>Longitude minutes</th>
-      <th>Environment</th>
-      <th>Habitat</th>
-      <th>Ph</th>
-      <th>Conductivity</th>
-      <th>Temperature</th>
-      <th>Salinity</th>
-      <th>Close picture</th>
-      <th>Laboratory picture</th>
-      <th>Collector</th>
-      <th>Collection date</th>
-      <th>Created at</th>
-      <th>Updated at</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($samples as $sample): ?>
-    <tr>
-      <td><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getId() ?></a></td>
-      <td><?php echo $sample->getNumber() ?></td>
-      <td><?php echo $sample->getEcosystemId() ?></td>
-      <td><?php echo $sample->getLocation() ?></td>
-      <td><?php echo $sample->getLatitudeDegrees() ?></td>
-      <td><?php echo $sample->getLongitudeDegrees() ?></td>
-      <td><?php echo $sample->getLatitudeMinutes() ?></td>
-      <td><?php echo $sample->getLongitudeMinutes() ?></td>
-      <td><?php echo $sample->getEnvironmentId() ?></td>
-      <td><?php echo $sample->getHabitatId() ?></td>
-      <td><?php echo $sample->getPh() ?></td>
-      <td><?php echo $sample->getConductivity() ?></td>
-      <td><?php echo $sample->getTemperature() ?></td>
-      <td><?php echo $sample->getSalinity() ?></td>
-      <td><?php echo $sample->getClosePicture() ?></td>
-      <td><?php echo $sample->getLaboratoryPicture() ?></td>
-      <td><?php echo $sample->getCollectorId() ?></td>
-      <td><?php echo $sample->getCollectionDate() ?></td>
-      <td><?php echo $sample->getCreatedAt() ?></td>
-      <td><?php echo $sample->getUpdatedAt() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
+	<thead>
+		<tr>
+			<th>Number</th>
+			<th>Location</th>
+			<th>Ecosystem</th>
+			<th>Environment</th>
+			<th>Habitat</th>
+			<th>Collector</th>
+			<th>Collection date</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($samples as $sample): ?>
+			<tr>
+				<td><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getNumber() ?></a></td>
+				<td><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getLocation() ?></a></td>
+				<td><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getEcosystemId() ?></a></td>
+				<td><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getEnvironmentId() ?></a></td>
+				<td><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getHabitatId() ?></a></td>
+				<td><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getCollectorId() ?></a></td>
+				<td><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getCollectionDate() ?></a></td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
 </table>
 
-  <a href="<?php echo url_for('sample/new') ?>">New</a>
+<a href="<?php echo url_for('sample/new') ?>">New</a>
--- lib/form/doctrine/SampleForm.class.php
+++ lib/form/doctrine/SampleForm.class.php
@@ -1,16 +1,36 @@
 <?php
 
 /**
- * Sample form.
- *
- * @package    bna_green_house
- * @subpackage form
- * @author     Eliezer Talon <elitalon@inventiaplus.com>
- * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
- */
+* Sample form.
+*
+* @package    bna_green_house
+* @subpackage form
+* @author     Eliezer Talon <elitalon@inventiaplus.com>
+* @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+*/
 class SampleForm extends BaseSampleForm
 {
-  public function configure()
-  {
-  }
+	public function configure()
+	{
+		$this->useFields(array(
+			'id',
+			'number',
+			'location',
+			'latitude_degrees',
+			'latitude_minutes',
+			'longitude_degrees',
+			'longitude_minutes',
+			'ecosystem_id',
+			'environment_id',
+			'habitat_id',
+			'ph',
+			'conductivity',
+			'temperature',
+			'salinity',
+			'close_picture',
+			'laboratory_picture',
+			'collector_id',
+			'collection_date',
+			));
+	}
 }
