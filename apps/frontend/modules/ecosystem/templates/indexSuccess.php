1e6651b5a3337716c7f770821845cf5b3666f399
Eliezer Talon <elitalon@inventiaplus.com>
2011-02-15 14:36:06 +0000

    Index action improved with selected columns

--- apps/frontend/modules/ecosystem/templates/indexSuccess.php
+++ apps/frontend/modules/ecosystem/templates/indexSuccess.php
@@ -1,24 +1,22 @@
-<h1>Ecosystems List</h1>
-
-<table>
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>City</th>
-      <th>Province</th>
-      <th>Lanscape picture</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($ecosystems as $ecosystem): ?>
-    <tr>
-      <td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo $ecosystem->getName() ?></a></td>
-      <td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo $ecosystem->getCity() ?></a></td>
-      <td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo $ecosystem->getProvinceId() ?></a></td>
-      <td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo $ecosystem->getLanscapePicture() ?></a></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
+<table id="ecosystems">
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>City</th>
+			<th>With picture?</th>
+			<th>Number of samples</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($ecosystems as $ecosystem): ?>
+			<tr>
+				<td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo $ecosystem->getName() ?></a></td>
+				<td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo $ecosystem->getCity() ?></a></td>
+				<td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo ($ecosystem->getPicture())?'yes':'no' ?></a></td>
+				<td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo count($ecosystem->getSamples()) ?></a></td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
 </table>
 
-  <a href="<?php echo url_for('ecosystem/new') ?>">New</a>
+<a href="<?php echo url_for('ecosystem/new') ?>">New</a>
