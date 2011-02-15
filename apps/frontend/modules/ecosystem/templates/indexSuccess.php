5e32400a0db4779bf7efc7de0208df16c93baccb
Eliezer Talon <elitalon@inventiaplus.com>
2011-02-15 15:34:17 +0000

    Index action improved

--- apps/frontend/modules/ecosystem/templates/indexSuccess.php
+++ apps/frontend/modules/ecosystem/templates/indexSuccess.php
@@ -1,4 +1,4 @@
-<table id="ecosystems">
+<table id="ecosystems_list">
 	<thead>
 		<tr>
 			<th>Name</th>
@@ -10,10 +10,10 @@
 	<tbody>
 		<?php foreach ($ecosystems as $ecosystem): ?>
 			<tr>
-				<td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo $ecosystem->getName() ?></a></td>
-				<td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo $ecosystem->getCity() ?></a></td>
-				<td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo ($ecosystem->getPicture())?'yes':'no' ?></a></td>
-				<td><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo count($ecosystem->getSamples()) ?></a></td>
+				<td id="ecosystems_list_name_column"><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo $ecosystem->getName() ?></a></td>
+				<td id="ecosystems_list_city_column"><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo $ecosystem->getCity() ?></a></td>
+				<td id="ecosystems_list_picture_column"><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo ($ecosystem->getPicture())?'yes':'no' ?></a></td>
+				<td id="ecosystems_list_count_column"><a href="<?php echo url_for('ecosystem/edit?id='.$ecosystem->getId()) ?>"><?php echo count($ecosystem->getSamples()) ?></a></td>
 			</tr>
 		<?php endforeach; ?>
 	</tbody>
