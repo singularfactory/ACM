fc17594892a3b1a24175d05a1a04f0e992804bfc
Eliezer Talon <elitalon@inventiaplus.com>
2011-02-15 18:50:45 +0000

    User-friendly sample list

--- apps/frontend/modules/sample/templates/indexSuccess.php
+++ apps/frontend/modules/sample/templates/indexSuccess.php
@@ -1,3 +1,4 @@
+<?php use_helper('Date') ?>
 <table id="samples_list">
 	<thead>
 		<tr>
@@ -13,10 +14,10 @@
 		<?php foreach ($samples as $sample): ?>
 			<tr>
 				<td id="samples_list_number_column"><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getNumber() ?></a></td>
-				<td id="samples_list_ecosystem_column"><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getEcosystemId() ?></a></td>
+				<td id="samples_list_ecosystem_column"><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getEcosystem()->getName() ?></a></td>
 				<td id="samples_list_location_column"><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getLocation() ?></a></td>
-				<td id="samples_list_collector_column"><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getCollectorId() ?></a></td>
-				<td id="samples_list_date_column"><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getCollectionDate() ?></a></td>
+				<td id="samples_list_collector_column"><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo $sample->getCollector()->getName() ?></a></td>
+				<td id="samples_list_date_column"><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo format_date($sample->getCollectionDate(), 'p') ?></a></td>
 				<td id="samples_list_count_column"><a href="<?php echo url_for('sample/show?id='.$sample->getId()) ?>"><?php echo '0' ?></a></td>
 			</tr>
 		<?php endforeach; ?>
