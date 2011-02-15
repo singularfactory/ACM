5c4e574ddf994834383c4abb9ad17c4fcb14b65a
Eliezer Talon <elitalon@inventiaplus.com>
2011-02-15 14:53:57 +0000

    Verbose _form partial added

--- apps/frontend/modules/ecosystem/templates/_form.php
+++ apps/frontend/modules/ecosystem/templates/_form.php
@@ -9,6 +9,7 @@
     <tfoot>
       <tr>
         <td colspan="2">
+          <?php echo $form->renderHiddenFields(false) ?>
           &nbsp;<a href="<?php echo url_for('ecosystem/index') ?>">Back to list</a>
           <?php if (!$form->getObject()->isNew()): ?>
             &nbsp;<?php echo link_to('Delete', 'ecosystem/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
@@ -18,7 +19,77 @@
       </tr>
     </tfoot>
     <tbody>
-      <?php echo $form ?>
+      <?php echo $form->renderGlobalErrors() ?>
+      <tr>
+        <th><?php echo $form['name']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['name']->renderError() ?>
+          <?php echo $form['name'] ?>
+        </td>
+      </tr>
+      <tr>
+        <th><?php echo $form['latitude_degrees']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['latitude_degrees']->renderError() ?>
+          <?php echo $form['latitude_degrees'] ?>
+        </td>
+      </tr>
+      <tr>
+        <th><?php echo $form['longitude_degrees']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['longitude_degrees']->renderError() ?>
+          <?php echo $form['longitude_degrees'] ?>
+        </td>
+      </tr>
+      <tr>
+        <th><?php echo $form['latitude_minutes']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['latitude_minutes']->renderError() ?>
+          <?php echo $form['latitude_minutes'] ?>
+        </td>
+      </tr>
+      <tr>
+        <th><?php echo $form['longitude_minutes']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['longitude_minutes']->renderError() ?>
+          <?php echo $form['longitude_minutes'] ?>
+        </td>
+      </tr>
+      <tr>
+        <th><?php echo $form['country_id']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['country_id']->renderError() ?>
+          <?php echo $form['country_id'] ?>
+        </td>
+      </tr>
+      <tr>
+        <th><?php echo $form['province_id']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['province_id']->renderError() ?>
+          <?php echo $form['province_id'] ?>
+        </td>
+      </tr>
+      <tr>
+        <th><?php echo $form['city']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['city']->renderError() ?>
+          <?php echo $form['city'] ?>
+        </td>
+      </tr>
+      <tr>
+        <th><?php echo $form['picture']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['picture']->renderError() ?>
+          <?php echo $form['picture'] ?>
+        </td>
+      </tr>
+      <tr>
+        <th><?php echo $form['remarks']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['remarks']->renderError() ?>
+          <?php echo $form['remarks'] ?>
+        </td>
+      </tr>
     </tbody>
   </table>
 </form>
