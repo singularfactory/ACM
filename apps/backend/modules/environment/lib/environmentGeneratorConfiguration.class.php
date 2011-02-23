2c52e22f85f08dbe2e6f680404271e76a4f60e31
Eliezer Talon <elitalon@inventiaplus.com>
2011-02-23 10:50:22 +0000

    Added admin modules for Habitat and Environment

---
+++ apps/backend/modules/environment/lib/environmentGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * environment module configuration.
+ *
+ * @package    bna_green_house
+ * @subpackage environment
+ * @author     Eliezer Talon <elitalon@inventiaplus.com>
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class environmentGeneratorConfiguration extends BaseEnvironmentGeneratorConfiguration
+{
+}
