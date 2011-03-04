20067abbd665997a3d8766324acd33b1643bd479
Eliezer Talon <elitalon@inventiaplus.com>
2011-03-04 15:27:48 +0000

    Added custom description to events (fixes 672)

--- apps/frontend/lib/eventLog.class.php
+++ apps/frontend/lib/eventLog.class.php
@@ -7,13 +7,34 @@ class EventLog
   static public function save_log_message(sfEvent $event_occurred)
   {
 	$notifier = $event_occurred->getSubject();
-	
+	$request = $notifier->getRequest();
 	$event = new Event();
-
+	
 	$event->setUserId($notifier->getUser()->getGuardUser()->getId());
-	$event->setIpAddress($notifier->getRequest()->getRemoteAddress());
+	$event->setIpAddress($request->getRemoteAddress());
 	$event->setAction($notifier->getActionName());
-	$event->setDescription($notifier->getModuleName());
+	
+	$module = $notifier->getModuleName();
+	$description = $request->getParameter('id');
+	switch ($module) {
+		case 'collector':
+		case 'country':
+		case 'environment':
+		case 'habitat':
+		case 'island':
+		case 'location':
+		case 'province':
+		case 'radiation':
+		case 'region':
+			$description += ';';
+			$description += $request->getParameter('name');
+			break;
+		case 'sample':
+			$description += ';';
+			$description += $request->getParameter('location_id');
+			break;
+	}
+	$event->setDescription("$module;$description");
 	
 	$event->save();	
   } 
