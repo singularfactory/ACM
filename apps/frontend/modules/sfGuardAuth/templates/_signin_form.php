f97d67824c9987f2d8a720c0d2918c2958ceb64c
Eliezer Talon <elitalon@inventiaplus.com>
2011-03-09 12:00:39 +0000

    Login form initial design

--- apps/frontend/modules/sfGuardAuth/templates/_signin_form.php
+++ apps/frontend/modules/sfGuardAuth/templates/_signin_form.php
@@ -1,23 +1,14 @@
 <form action="<?php echo url_for('@login') ?>" method="post">
-  <table>
-    <tbody>
-      <?php echo $form ?>
-    </tbody>
-    <tfoot>
-      <tr>
-        <td colspan="2">
-          <input type="submit" value="<?php echo __('Signin', null, 'sf_guard') ?>" />
-          
-          <?php $routes = $sf_context->getRouting()->getRoutes() ?>
-          <?php if (isset($routes['sf_guard_forgot_password'])): ?>
-            <a href="<?php echo url_for('@sf_guard_forgot_password') ?>"><?php echo __('Forgot your password?', null, 'sf_guard') ?></a>
-          <?php endif; ?>
-
-          <?php if (isset($routes['sf_guard_register'])): ?>
-            &nbsp; <a href="<?php echo url_for('@sf_guard_register') ?>"><?php echo __('Want to register?', null, 'sf_guard') ?></a>
-          <?php endif; ?>
-        </td>
-      </tr>
-    </tfoot>
-  </table>
+	<div id="username">
+		<?php echo $form['username']->renderLabel() ?>
+		<?php echo $form['username']; ?>
+	</div>
+	<div id="password">
+		<?php echo $form['password']->renderLabel() ?>
+		<?php echo $form['password']; ?>
+	</div>
+	<div id="submit">
+		<input type="submit" value="Sign in">
+	</div>
+	<?php echo $form->renderHiddenFields() ?>
 </form>
