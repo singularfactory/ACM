bd9ba66f039b701598d4e8423d001e5a9990fdbd
Eliezer Talon <elitalon@inventiaplus.com>
2011-03-09 08:17:11 +0000

    Made logger event handler application-wide (refs 672)

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -9,5 +9,8 @@ class ProjectConfiguration extends sfProjectConfiguration
   {
     $this->enablePlugins('sfDoctrinePlugin');
     $this->enablePlugins('sfDoctrineGuardPlugin');
+
+	// Register the logger event handler
+	$this->dispatcher->connect('bna_green_house.event_log', array('EventLog', 'save_log_message'));
   }
 }
