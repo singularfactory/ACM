c2dd8aa15da37cf748b613182981ca697b878970
Eliezer Talon <elitalon@inventiaplus.com>
2011-03-16 08:58:31 +0000

    Administration of permissions (refs 683)

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -64,6 +64,9 @@
 					<li>
 						<?php echo link_to('Roles', '@sf_guard_group', (preg_match('/^sf_guard_group_?/', $route))?array('class' => 'header_menu_current_tab'):'') ?>
 					</li>
+					<li>
+						<?php echo link_to('Permissions', '@sf_guard_permission', (preg_match('/^sf_guard_permission_?/', $route))?array('class' => 'header_menu_current_tab'):'') ?>
+					</li>
 					
 					<li class="header_menu_right_tab">
 						<?php echo link_to('Back to application', '/') ?>
