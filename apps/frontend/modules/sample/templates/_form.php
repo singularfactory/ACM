4049de955575524db8693eb03f6739af90ca36c7
Eliezer Talon <elitalon@inventiaplus.com>
2011-03-21 12:16:21 +0000

    Sample form improved (refs 758, fixes 681)

--- apps/frontend/modules/sample/templates/_form.php
+++ apps/frontend/modules/sample/templates/_form.php
@@ -2,20 +2,127 @@
 <?php use_javascripts_for_form($form) ?>
 
 <?php echo form_tag_for($form, '@sample') ?>
-<table id="sample_form">
-	<tfoot>
-		<tr>
-			<td colspan="2">
-				<?php echo link_to('Back to all samples', 'sample/index') ?>
-				<?php if (!$form->getObject()->isNew()): ?>
-					&nbsp;<?php echo link_to('Delete', 'sample/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
-				<?php endif; ?>
-				<input type="submit" value="Save" />
-			</td>
-		</tr>
-	</tfoot>
-	<tbody>
-		<?php echo $form; ?>
-	</tbody>
-</table>
+	<?php echo $form->renderHiddenFields() ?>
+
+	<div id="id">
+		<?php echo $form['id']->renderLabel() ?>
+		<?php echo $form['id']->renderHelp() ?>
+		<?php echo $form['id'] ?>
+	</div>
+
+	<div id="location">
+		<?php echo $form['location_id']->renderLabel() ?>
+		<?php echo $form['location_id']->renderHelp() ?>
+		<?php echo $form['location_id'] ?>
+	</div>
+	
+	<div id="latitude">
+		<?php echo $form['latitude']->renderLabel() ?>
+		<?php echo $form['latitude']->renderHelp() ?>
+		<?php echo $form['latitude'] ?>
+	</div>
+	
+	<div id="longitude">
+		<?php echo $form['longitude']->renderLabel() ?>
+		<?php echo $form['longitude']->renderHelp() ?>
+		<?php echo $form['longitude'] ?>
+	</div>
+
+	<div id="environment">
+		<?php echo $form['environment_id']->renderLabel() ?>
+		<?php echo $form['environment_id']->renderHelp() ?>
+		<?php echo $form['environment_id'] ?>
+	</div>
+
+	<div id="habitat">
+		<?php echo $form['habitat_id']->renderLabel() ?>
+		<?php echo $form['habitat_id']->renderHelp() ?>
+		<?php echo $form['habitat_id'] ?>
+	</div>
+
+	<div id="extremophile">
+		<?php echo $form['is_extremophile']->renderLabel() ?>
+		<?php echo $form['is_extremophile']->renderHelp() ?>
+		<?php echo $form['is_extremophile'] ?>
+	</div>
+
+	<div id="ph">
+		<?php echo $form['ph']->renderLabel() ?>
+		<?php echo $form['ph']->renderHelp() ?>
+		<?php echo $form['ph'] ?>
+	</div>
+
+	<div id="conductivity">
+		<?php echo $form['conductivity']->renderLabel() ?>
+		<?php echo $form['conductivity']->renderHelp() ?>
+		<?php echo $form['conductivity'] ?>
+	</div>
+
+	<div id="temperature">
+		<?php echo $form['temperature']->renderLabel() ?>
+		<?php echo $form['temperature']->renderHelp() ?>
+		<?php echo $form['temperature'] ?>
+	</div>
+
+	<div id="salinity">
+		<?php echo $form['salinity']->renderLabel() ?>
+		<?php echo $form['salinity']->renderHelp() ?>
+		<?php echo $form['salinity'] ?>
+	</div>
+
+	<div id="altitude">
+		<?php echo $form['altitude']->renderLabel() ?>
+		<?php echo $form['altitude']->renderHelp() ?>
+		<?php echo $form['altitude'] ?>
+	</div>
+
+	<div id="radiation">
+		<?php echo $form['radiation_id']->renderLabel() ?>
+		<?php echo $form['radiation_id']->renderHelp() ?>
+		<?php echo $form['radiation_id'] ?>
+	</div>
+
+	<div id="field_picture">
+		<?php echo $form['field_picture']->renderLabel() ?>
+		<?php echo $form['field_picture']->renderHelp() ?>
+		<?php echo $form['field_picture'] ?>
+	</div>
+
+	<div id="detailed_picture">
+		<?php echo $form['detailed_picture']->renderLabel() ?>
+		<?php echo $form['detailed_picture']->renderHelp() ?>
+		<?php echo $form['detailed_picture'] ?>
+	</div>
+
+	<div id="microscopic_picture">
+		<?php echo $form['microscopic_picture']->renderLabel() ?>
+		<?php echo $form['microscopic_picture']->renderHelp() ?>
+		<?php echo $form['microscopic_picture'] ?>
+	</div>
+
+	<div id="collector">
+		<?php echo $form['collector_id']->renderLabel() ?>
+		<?php echo $form['collector_id']->renderHelp() ?>
+		<?php echo $form['collector_id'] ?>
+	</div>
+
+	<div id="collection_date">
+		<?php echo $form['collection_date']->renderLabel() ?>
+		<?php echo $form['collection_date']->renderHelp() ?>
+		<?php echo $form['collection_date'] ?>
+	</div>
+	
+	<div id="remarks">
+		<?php echo $form['remarks']->renderLabel() ?>
+		<?php echo $form['remarks'] ?>
+	</div>
+
+	<div class="submit">
+		<?php if ( $form->getObject()->isNew() ): ?>
+			<input type="submit" value="Create this sample"> 
+		<?php else: ?>
+			<input type="submit" value="Save changes">
+		<?php endif; ?>	
+		or <?php echo link_to('cancel', 'sample/index', array('class' => 'cancel_form_link')) ?>
+	</div>
 </form>
