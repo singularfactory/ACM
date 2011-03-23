c3396d9d16e88b0a1af17c5e3e8edb5c907311ae
Eliezer Talon <elitalon@inventiaplus.com>
2011-03-23 12:58:00 +0000

    JQuery installed (refs 761)

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -6,6 +6,7 @@
 		<?php include_title() ?>
 		<link rel="shortcut icon" href="/favicon.ico" />
 		<?php include_stylesheets() ?>
+		<?php use_javascript('jquery.js') ?>
 		<?php include_javascripts() ?>
 	</head>
 	<body>
