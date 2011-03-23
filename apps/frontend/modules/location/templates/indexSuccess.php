898fb74601fea7aa1e50ae66bebf743dad08e909
Eliezer Talon <elitalon@inventiaplus.com>
2011-03-23 09:56:16 +0000

    Added links to every table row (refs 763)

--- apps/frontend/modules/location/templates/indexSuccess.php
+++ apps/frontend/modules/location/templates/indexSuccess.php
@@ -14,20 +14,23 @@
 			<th><?php echo link_to('Region', 'location/index?sort=Region.name&sort_type='.$sortType) ?></th>
 			<th><?php echo link_to('Island', 'location/index?sort=Island.name&sort_type='.$sortType) ?></th>
 			<th>Samples</th>
-			<th class="actions"></th>
+			<th></th>
 		</tr>
 
 		<?php foreach ($pager->getResults() as $location): ?>
 		<tr>
-			<td class="location_name"><?php echo $location->getName() ?></td>
-			<td class="country_name"><?php echo $location->getCountry()->getName() ?></td>
-			<td class="region_name"><?php echo $location->getRegion()->getName() ?></td>
-			<td class="island_name"><?php echo ($location->getIslandId())?$location->getIsland()->getName():'-'; ?></td>
-			<td class="object_count"><?php echo $location->getNbSamples() ?></td>
+			<?php $url = url_for('@location_show?id='.$location->getId()) ?>			
+			<td class="location_name"><?php echo link_to($location->getName(), $url) ?></td>
+			<td class="country_name"><?php echo link_to($location->getCountry()->getName(), $url) ?></td>
+			<td class="region_name"><?php echo link_to($location->getRegion()->getName(), $url) ?></td>
+			<td class="island_name"><?php echo link_to(($location->getIslandId())?$location->getIsland()->getName():'-', $url) ?></td>
+			<td class="object_count"><?php echo link_to($location->getNbSamples(), $url) ?></td>
 		
 			<td class="actions">
-				<?php echo link_to('Edit', 'location/edit?id='.$location->getId()) ?>
-				<?php echo link_to('Delete', 'location/delete?id='.$location->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
+				<a href="<?php echo $url ?>">
+					<?php echo link_to('Edit', 'location/edit?id='.$location->getId()) ?>
+					<?php echo link_to('Delete', 'location/delete?id='.$location->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
+				</a>
 			</td>
 		</tr>
 		<?php endforeach; ?>
