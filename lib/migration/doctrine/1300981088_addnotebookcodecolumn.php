4d8610878f9fec06f2cb25e78e57ceeca379ef9a
Eliezer Talon <elitalon@inventiaplus.com>
2011-03-24 15:41:05 +0000

    Migration to add the notebook_code column

---
+++ lib/migration/doctrine/1300981088_addnotebookcodecolumn.php
@@ -0,0 +1,12 @@
+<?php
+
+class AddNotebookCodeColumn extends Doctrine_Migration_Base
+{
+  public function up() {
+	$this->addColumn('sample', 'notebook_code', 'integer', 8, array('notnull' => true));
+  }
+
+  public function down() {
+	$this->removeColumn('sample', 'notebook_code');
+  }
+}
