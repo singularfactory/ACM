bb72d1a24c607fffc171b43a9ea251e470e428a9
Eliezer Talon <elitalon@inventiaplus.com>
2011-03-28 16:39:16 +0100

    CSS styles and messages of marker windows improved (refs 763)

--- apps/frontend/lib/myGoogleMap.class.php
+++ apps/frontend/lib/myGoogleMap.class.php
@@ -62,10 +62,10 @@ class MyGoogleMap extends GMap {
 			array('icon' => new GMapMarkerImage(sfConfig::get('app_map_pictures_directory').'/location.png', array('width' => 18, 'height' => 25))));
 		
 		if ( array_key_exists('notes', $window_options) ) {
-			$window_options['notes'] .= '<br />This location is <strong>not accurate</strong>. GPS coordinates were calculated from available information';
+			$window_options['notes'] .= '<br />This location is <strong>not accurate</strong>. GPS coordinates were estimated from available information';
 		}
 		else {
-			$window_options['notes'] = '<br />This location is <strong>not accurate</strong>. GPS coordinates were calculated from available information';
+			$window_options['notes'] = '<br />This location is <strong>not accurate</strong>. GPS coordinates were estimated from available information';
 		}
 		$marker->addHtmlInfoWindow($this->buildMarkerWindow($window_options));
 		return $marker;
