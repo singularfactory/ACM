356bd16ab10bf4e472ea51ce0080ac1dcae48ef8
Eliezer Talon <elitalon@inventiaplus.com>
2011-04-13 09:38:20 +0100

    sfDependentSelectPlugin installed (refs 809)

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,19 +1,19 @@
 <?php
-
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
-class ProjectConfiguration extends sfProjectConfiguration
-{
-  public function setup()
-  {
-    $this->enablePlugins('sfDoctrinePlugin');
-    $this->enablePlugins('sfDoctrineGuardPlugin');
+class ProjectConfiguration extends sfProjectConfiguration {
+	public function setup() {
+		$this->enablePlugins(array(
+			'sfDoctrinePlugin',
+			'sfDoctrineGuardPlugin',
+			'ahDoctrineEasyEmbeddedRelationsPlugin',
+			'sfEasyGMapPlugin',
+			'sfThumbnailPlugin',
+			'sfDependentSelectPlugin',
+		));
 
-	// Register the logger event handler
-	$this->dispatcher->connect('bna_green_house.event_log', array('EventLog', 'save_log_message'));
-    $this->enablePlugins('ahDoctrineEasyEmbeddedRelationsPlugin');
-	$this->enablePlugins('sfEasyGMapPlugin');
-    $this->enablePlugins('sfThumbnailPlugin');
-  }
+		// Register the logger event handler
+		$this->dispatcher->connect('bna_green_house.event_log', array('EventLog', 'save_log_message'));
+	}
 }
