29480d16917356cda56a4bb3265e675ac437e7cc
Eliezer Talon <elitalon@inventiaplus.com>
2011-06-30 11:17:34 +0100

    growth medium form customized

--- apps/frontend/modules/growth_medium/templates/_form.php
+++ apps/frontend/modules/growth_medium/templates/_form.php
@@ -1,53 +1,42 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
 
-<form action="<?php echo url_for('growth_medium/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
-<?php if (!$form->getObject()->isNew()): ?>
-<input type="hidden" name="sf_method" value="put" />
-<?php endif; ?>
-  <table>
-    <tfoot>
-      <tr>
-        <td colspan="2">
-          <?php echo $form->renderHiddenFields(false) ?>
-          &nbsp;<a href="<?php echo url_for('growth_medium/index') ?>">Back to list</a>
-          <?php if (!$form->getObject()->isNew()): ?>
-            &nbsp;<?php echo link_to('Delete', 'growth_medium/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
-          <?php endif; ?>
-          <input type="submit" value="Save" />
-        </td>
-      </tr>
-    </tfoot>
-    <tbody>
-      <?php echo $form->renderGlobalErrors() ?>
-      <tr>
-        <th><?php echo $form['name']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['name']->renderError() ?>
-          <?php echo $form['name'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['description']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['description']->renderError() ?>
-          <?php echo $form['description'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['link']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['link']->renderError() ?>
-          <?php echo $form['link'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['strains_list']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['strains_list']->renderError() ?>
-          <?php echo $form['strains_list'] ?>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+<?php echo form_tag_for($form, '@growth_medium') ?>
+	<?php echo $form->renderHiddenFields() ?>
+
+	<div id="left_side_form">
+		<div id="name">
+			<?php echo $form['name']->renderLabel() ?>
+			<?php echo $form['name']->renderError() ?>
+			<?php echo $form['name']->renderHelp() ?>
+			<?php echo $form['name'] ?>
+		</div>
+
+		<div id="description">
+			<?php echo $form['description']->renderLabel() ?>
+			<?php echo $form['description']->renderError() ?>
+			<?php echo $form['description']->renderHelp() ?>
+			<?php echo $form['description'] ?>
+		</div>
+		
+		<div id="link">
+			<?php echo $form['link']->renderLabel() ?>
+			<?php echo $form['link']->renderError() ?>
+			<?php echo $form['link']->renderHelp() ?>
+			<?php echo $form['link'] ?>
+		</div>
+	</div>
+
+	<div id="right_side_form">
+	</div>
+	
+	<div class="submit">
+		<?php if ( $form->getObject()->isNew() ): ?>
+			<input type="submit" value="Create this growth medium">
+			<input type="submit" name="_save_and_add" value="Create and add">
+		<?php else: ?>
+			<input type="submit" value="Save changes">
+		<?php endif; ?>	
+		or <?php echo link_to('cancel', $sf_request->getReferer(), array('class' => 'cancel_form_link')) ?>
+	</div>
 </form>
--- lib/form/doctrine/GrowthMediumForm.class.php
+++ lib/form/doctrine/GrowthMediumForm.class.php
@@ -9,6 +9,9 @@
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class GrowthMediumForm extends BaseGrowthMediumForm {
-  public function configure() {
-  }
+	public function configure() {
+		$this->widgetSchema->setHelp('name', 'Name of this growth medium');
+		$this->widgetSchema->setHelp('description', 'A detailed explanation of characteristics of this growth medium');
+		$this->widgetSchema->setHelp('link', 'Links to external resources');
+	}
 }
