7e78aadce4148b51e33eaa3b94792cd06195163a
Eliezer Talon <elitalon@inventiaplus.com>
2011-07-04 12:22:19 +0100

    Back to list links improved by adding search terms if any

--- apps/frontend/templates/_back_header_action.php
+++ apps/frontend/templates/_back_header_action.php
@@ -1,3 +1,21 @@
+<?php
+	$pageNumber = null;
+	if ( $page = $sf_user->getAttribute("$module.index_page") ) {
+		$pageNumber = "?page=$page";
+	}
+	
+	$searchCriteria = null;
+	if ( $text = $sf_user->getAttribute('search.criteria') ) {
+		$searchCriteria = "criteria=$text";
+		if ( !$pageNumber ) {
+			$searchCriteria = "?$searchCriteria";
+		}
+		else {
+			$searchCriteria = "&$searchCriteria";
+		}
+	}	
+?>
+
 <div id="main_header_action_back" class="main_header_action">
-	<?php echo link_to('Back to list', "@$module?page=".$sf_user->getAttribute("$module.index_page")) ?>
+	<?php echo link_to('Back to list', "@$module{$pageNumber}{$searchCriteria}") ?>
 </div>
