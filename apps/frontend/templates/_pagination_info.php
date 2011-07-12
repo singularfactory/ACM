f29148a3f9e4aa844aca45d988baffd33b013775
Eliezer Talon <elitalon@inventiaplus.com>
2011-07-12 14:07:32 +0100

    Extra padding in links and table columns removed. Background from pagination buttons cleared

--- apps/frontend/templates/_pagination_info.php
+++ apps/frontend/templates/_pagination_info.php
@@ -9,14 +9,14 @@
 	<?php echo count($pager) ?> items - showing page <?php echo $pager->getPage() ?> of <?php echo $pager->getLastPage() ?>
 
 	<span id="pagination_links">
-		<?php echo link_to(image_tag('first.png', array('alt' => 'First page', 'title' => 'First page')), $url.$pager->getFirstPage()) ?>
-		<?php echo link_to(image_tag('previous.png', array('alt' => 'Previous page', 'title' => 'Previous page')), $url.$pager->getPreviousPage()) ?>
+		<?php echo link_to(image_tag('first.png', array('alt' => 'First page', 'title' => 'First page')), $url.$pager->getFirstPage(), array('class' => 'button')) ?>
+		<?php echo link_to(image_tag('previous.png', array('alt' => 'Previous page', 'title' => 'Previous page')), $url.$pager->getPreviousPage(), array('class' => 'button')) ?>
 
 		<?php foreach ($pager->getLinks() as $page): ?>
 		<?php echo ($page == $pager->getPage()) ? $page : link_to($page, $url.$page) ?>
 		<?php endforeach ?>
 		
-		<?php echo link_to(image_tag('next.png', array('alt' => 'Next page', 'title' => 'Next page')), $url.$pager->getNextPage()) ?>
-		<?php echo link_to(image_tag('last.png', array('alt' => 'Last page', 'title' => 'Last page')), $url.$pager->getLastPage()) ?>
+		<?php echo link_to(image_tag('next.png', array('alt' => 'Next page', 'title' => 'Next page')), $url.$pager->getNextPage(), array('class' => 'button')) ?>
+		<?php echo link_to(image_tag('last.png', array('alt' => 'Last page', 'title' => 'Last page')), $url.$pager->getLastPage(), array('class' => 'button')) ?>
 	</span>
 </div>
