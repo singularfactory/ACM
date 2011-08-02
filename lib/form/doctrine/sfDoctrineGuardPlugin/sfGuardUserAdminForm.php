1b890e7209fa3f36afa01ee8e3450b32f9d0583d
Eliezer Talon <elitalon@inventiaplus.com>
2011-08-02 11:04:36 +0100

    Group list removed from Users section in backend

--- lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardUserAdminForm.php
+++ lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardUserAdminForm.php
@@ -1,19 +1,20 @@
 <?php
 
 /**
- * Custom sfGuardUserAdminForm for admin generators
- *
- * @package    sfDoctrineGuardPlugin
- * @subpackage form
- * @author     Eliezer Talon <elitalon@inventiaplus.com>
- */
+* Custom sfGuardUserAdminForm for admin generators
+*
+* @package    sfDoctrineGuardPlugin
+* @subpackage form
+* @author     Eliezer Talon <elitalon@inventiaplus.com>
+*/
 class sfGuardUserAdminForm extends BasesfGuardUserAdminForm {
-  public function configure() {
-	unset($this['permissions_list']);
 	
-	$this->widgetSchema->setLabel('first_name', 'Name');
-	$this->widgetSchema->setLabel('last_name', 'Surname');
-	$this->widgetSchema->setLabel('email_address', 'Email');
-	$this->widgetSchema->setLabel('is_super_admin', 'Is super admin?');
-  }
+	public function configure() {
+		unset($this['permissions_list'], $this['groups_list']);
+		
+		$this->widgetSchema->setLabel('first_name', 'Name');
+		$this->widgetSchema->setLabel('last_name', 'Surname');
+		$this->widgetSchema->setLabel('email_address', 'Email');
+		$this->widgetSchema->setLabel('is_super_admin', 'Is super admin?');
+	}
 }
