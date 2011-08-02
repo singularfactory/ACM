fc59a56284794b5c956b78b3f1e75725765b0653
Eliezer Talon <elitalon@inventiaplus.com>
2011-08-02 11:29:38 +0100

    Only superadmin can set/unset superadmin flag

--- lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardUserAdminForm.php
+++ lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardUserAdminForm.php
@@ -12,9 +12,16 @@ class sfGuardUserAdminForm extends BasesfGuardUserAdminForm {
 	public function configure() {
 		unset($this['permissions_list'], $this['groups_list']);
 		
+		// Only superadministrators are allowed to set/unset the superadministrator flag
+		if ( !sfContext::getInstance()->getUser()->isSuperAdmin() ) {
+			unset($this['is_super_admin']);
+		}
+		else {
+			$this->widgetSchema->setLabel('is_super_admin', 'Is super admin?');
+		}
+		
 		$this->widgetSchema->setLabel('first_name', 'Name');
 		$this->widgetSchema->setLabel('last_name', 'Surname');
 		$this->widgetSchema->setLabel('email_address', 'Email');
-		$this->widgetSchema->setLabel('is_super_admin', 'Is super admin?');
 	}
 }
