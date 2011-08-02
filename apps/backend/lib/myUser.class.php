39c237f413edf993173910ea164902db3cdccd84
Eliezer Talon <elitalon@inventiaplus.com>
2011-08-02 10:52:33 +0100

    Administration role removed from backend credentials

--- apps/backend/lib/myUser.class.php
+++ apps/backend/lib/myUser.class.php
@@ -1,4 +1,18 @@
 <?php
 
 class myUser extends sfGuardSecurityUser {
+	
+	/**
+	 * Checks if logged user has a certain credential
+	 *
+	 * @param string $credential 
+	 * @param boolean $useAnd 
+	 * @return boolean
+	 * @author Eliezer Talon
+	 * @version	2011-08-02
+	*/
+	public function hasCredential($credential, $useAnd = true) {
+		// combine the credential and the permissions check
+		return (parent::hasCredential($credential, $useAnd) || parent::hasPermission($credential));
+	}
 }
--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -2,4 +2,17 @@
 
 class myUser extends sfGuardSecurityUser {
 	
+	/**
+	 * Checks if logged user has a certain credential
+	 *
+	 * @param string $credential 
+	 * @param boolean $useAnd 
+	 * @return boolean
+	 * @author Eliezer Talon
+	 * @version	2011-08-02
+	*/
+	public function hasCredential($credential, $useAnd = true) {
+		// combine the credential and the permissions check
+		return (parent::hasCredential($credential, $useAnd) || parent::hasPermission($credential));
+	}
 }
