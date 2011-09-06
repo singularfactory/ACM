281f6985b0e0840a00518c071fdb48c94fdea133
Eliezer Talon <elitalon@inventiaplus.com>
2011-09-06 10:14:34 +0100

    Added Doctrine cache

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -3,6 +3,7 @@ require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutolo
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration {
+	
 	public function setup() {
 		$this->enablePlugins(array(
 			'sfDoctrinePlugin',
@@ -14,5 +15,12 @@ class ProjectConfiguration extends sfProjectConfiguration {
 
 		// Register the logger event handler
 		$this->dispatcher->connect('bna_green_house.event_log', array('EventLog', 'save_log_message'));
+	}
+	
+	/**
+	 * Configure the Doctrine engine
+	 **/
+	public function configureDoctrine(Doctrine_Manager $manager) {
+	  $manager->setAttribute(Doctrine::ATTR_QUERY_CACHE, new Doctrine_Cache_Apc());
 	}
 }
