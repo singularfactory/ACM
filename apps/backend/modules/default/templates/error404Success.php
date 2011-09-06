749c96a56c8f11c8806abdd6c1c806dd1ca4bb3d
Eliezer Talon <elitalon@inventiaplus.com>
2011-09-06 14:48:09 +0100

    Fixed back link

--- apps/backend/modules/default/templates/error404Success.php
+++ apps/backend/modules/default/templates/error404Success.php
@@ -5,7 +5,7 @@
 				<div id="http_error_text">
 					<h2>Page not found</h2>
 					<p>Don't freak out. Everything is going to be OK</p>
-					<p class="navigation">Try to <a href="#" id="go_back_link">go back</a>.</p>
+					<p class="navigation">Try to <a href="#" id="go_back_link" onclick="window.history.back(); return false;">go back</a>.</p>
 				</div>
 				<div id="http_error_image">
 					<?php echo image_tag('http_error.png', array('alt' => 'Error 404', 'width' => 290, 'height' => 308)) ?>
