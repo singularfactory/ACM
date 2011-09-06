520e220716363562615284c294f19d9140caaa84
Eliezer Talon <elitalon@inventiaplus.com>
2011-09-06 10:02:34 +0100

    Custom unavailable page

---
+++ config/unavailable.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+	<head> 
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+		<meta name="title" content="Algae culture management | Banco Español de Algas" /> 
+		<meta name="description" content="Algae culture management of Banco Español de Algas" /> 
+		<meta name="keywords" content="algae, bna, bea, banco, nacional, algas, español, marine, biotechnology, strains, culture" /> 
+		<meta name="language" content="en" /> 
+		<meta name="robots" content="index, follow" /> 
+		<title>Algae culture management | Banco Español de Algas</title> 
+		<link rel="shortcut icon" href="/favicon.ico" /> 
+		<link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" /> 
+		<script type="text/javascript" src="/js/jquery.js"></script> 
+		<script type="text/javascript" src="/js/main.js"></script>
+	</head> 
+	<body> 
+		<noscript> 
+			<div class="javascript_disabled_notification">
+				<div class="inner">
+					<h1>You need to change a setting in your web browser</h1>
+					<p>This application requires a browser feature called <strong>JavaScript</strong>. All modern browsers support JavaScript. You probably just need to change a setting in order to turn it on.</p>
+					<p>Please see: <a href="http://www.google.com/support/bin/answer.py?answer=23852">How to enable JavaScript in your browser</a>.</p>
+					<p>If you use ad-blocking software, it may require you to allow JavaScript from basecamphq.com.</p>
+					<p>Once you've enabled JavaScript you can <a href="">try loading this page again</a>.</p>
+					<p>Thank you.</p>
+				</div>
+			</div>
+		</noscript> 
+		
+		<div id="header"> 
+			<img id="header_image" alt="Banco Español de Algas" src="/images/logo.png" />
+			<h1 id="header_title">Algae culture management</h1> 
+			<h2 id="header_subtitle">Banco Español de Algas</h2> 				
+			<div id="header_shortcuts"></div> 
+			<div id="header_menu"></div> 
+		</div> 
+ 
+		<div id="main"> 
+			<div id="main_content"> 
+				<div id="main_header" style="display: none"></div> 
+				<div id="main_view"> 
+					<div class="http_error">
+						<div id="http_error_text">
+							<h2>Oops! Website temporarily unavailable</h2>
+							<p>Don't freak out. Everything is going to be OK</p>
+							<p class="navigation">Wait a few seconds and try to <a href="#" id="go_back_link">go back</a>.</p>
+						</div>
+						<div id="http_error_image">
+							<img alt="Website temporarily unavailable" src="/images/http_error.png" width="290" height="308" />
+						</div>
+					</div>
+				</div> 
+			</div> 
+			<div id="main_actions"></div> 
+		</div> 
+ 
+		<div id="footer"></div>
+	</body> 
+</html>
