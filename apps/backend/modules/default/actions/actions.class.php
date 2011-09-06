7d5a77e6a243f81dda896a5636d8da74f3881107
Eliezer Talon <elitalon@inventiaplus.com>
2011-09-06 14:41:18 +0100

    added 404 error page in backend

---
+++ apps/backend/modules/default/actions/actions.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * default actions.
+ *
+ * @package    bna_green_house
+ * @subpackage strain
+ * @author     Eliezer Talon <elitalon@inventiaplus.com>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class defaultActions extends MyActions {
+
+	/**
+	 * Error page for page not found (404) error
+	 */
+	public function executeError404() { }
+	
+}
---
+++ apps/backend/modules/default/templates/error404Success.php
@@ -0,0 +1,17 @@
+<div id="main">
+	<div id="main_content">
+		<div id="main_view">
+			<div class="http_error">
+				<div id="http_error_text">
+					<h2>Page not found</h2>
+					<p>Don't freak out. Everything is going to be OK</p>
+					<p class="navigation">Try to <a href="#" id="go_back_link">go back</a>.</p>
+				</div>
+				<div id="http_error_image">
+					<?php echo image_tag('http_error.png', array('alt' => 'Error 404', 'width' => 290, 'height' => 308)) ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="main_actions"></div>
+</div>
