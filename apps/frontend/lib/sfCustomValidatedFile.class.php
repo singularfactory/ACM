a81f237dd4a21670defa5b491bb684a518090973
Eliezer Talon <elitalon@inventiaplus.com>
2011-09-07 12:07:26 +0100

    Uploaded pictures automatically converted to PNG

--- apps/frontend/lib/sfCustomValidatedFile.class.php
+++ apps/frontend/lib/sfCustomValidatedFile.class.php
@@ -5,7 +5,8 @@ class sfCustomValidatedFile extends sfValidatedFile {
 		// Let the parent class save the file and do what it normally does
 		$filename = parent::save($file, $fileMode, $create, $dirMode);
 		
-		// Create the thumnbail if the file was successfully saved
+		// Create the thumbnail if the file was successfully saved
+		$pngPictureFilename = $filename;
 		if ( $this->isSaved() ) {
 			// Add support for alternative installation of ImageMagick binaries
 			$PATH=getenv('PATH');
@@ -23,9 +24,27 @@ class sfCustomValidatedFile extends sfValidatedFile {
 			$thumbnail->loadFile("$path/$filename");
 			$thumbnailFilename = preg_replace('/\.[\w\-]+$/', sfConfig::get('app_thumbnail_extension'), $filename);
 			$thumbnail->save("$thumbnailsDirectory/$thumbnailFilename", 'image/png');
+			
+			// Create a PNG version of the picture
+			$pngPicture = new Imagick("$path/$filename");
+			$pngPicture->setImageUnits(imagick::RESOLUTION_PIXELSPERINCH);
+			$pngPicture->setResolution(sfConfig::get('app_picture_resolution'), sfConfig::get('app_picture_resolution'));
+			
+			$pngPictureFilename = preg_replace('/\.[\w\-]+$/', sfConfig::get('app_picture_extension'), $filename);
+			if ( !$pngPicture->writeImage("$path/$pngPictureFilename") ) {
+				$pngPictureFilename = $filename;
+			}
+			else {
+				// Delete the uploaded file
+				if ( !preg_match('/\.png$/', $filename) ) {
+					unlink("$path/$filename");
+				}
+			}
+			$pngPicture->clear();
+			$pngPicture->destroy();
 		}
 		
 		// Return the saved file as normal
-		return $filename;
+		return $pngPictureFilename;
 	}
 }
