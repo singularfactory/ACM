c1aceea6fc85350710f605ebe061e95f8840db60
Eliezer Talon <elitalon@inventiaplus.com>
2011-10-19 14:37:59 +0100

    Added a partial to include submit buttons at the end of forms

---
+++ apps/frontend/templates/_submit_form_div.php
@@ -0,0 +1,20 @@
+<?php if ( isset($form) && isset($module) ): ?>
+
+	<?php if ( !isset($add) ) $add = true ?>
+	<?php $route = "@$module" ?>
+	
+	<div class="submit">
+		<?php if ( $form->getObject()->isNew() ): ?>
+			<input type="submit" value="Create this isolation">
+			
+			<?php if ( $add ): ?><input type="submit" name="_save_and_add" value="Create and add"><?php endif ?>
+			
+		<?php else: ?>
+			<?php $route = sprintf('@%s_show?id=%d', $module, $form->getObject()->getId()) ?>
+			<input type="submit" value="Save changes">
+		<?php endif; ?>	
+
+		or <?php echo link_to('cancel', $route, array('class' => 'cancel_form_link')) ?>
+	</div>
+
+<?php endif ?>
