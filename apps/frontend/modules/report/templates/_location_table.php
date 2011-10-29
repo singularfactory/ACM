2c184e289389d233a6593ddf8a342273e4c20654
Eliezer Talon <elitalon@inventiaplus.com>
2011-10-29 08:54:25 +0100

    Do not display columns that were used as filters

--- apps/frontend/modules/report/templates/_location_table.php
+++ apps/frontend/modules/report/templates/_location_table.php
@@ -42,22 +42,42 @@
 			</dl>
 		<?php endif ?>
 	</div>		
-
+	
 	<table id="report_results_list">
 		<tbody>
 			<tr>
 				<th>Location</th>
+
+				<?php if ( !$filters->offsetExists('Country') ): ?>
 				<th>Country</th>
+				<?php endif ?>
+				
+				<?php if ( !$filters->offsetExists('Region') ): ?>
 				<th>Region</th>
+				<?php endif ?>
+				
+				<?php if ( !$filters->offsetExists('Island') ): ?>
 				<th>Island</th>
+				<?php endif ?>
+				
 				<th class="report_object_count">Samples</th>
 			</tr>
 			<?php foreach ( $results as $location ): ?>
 			<tr>
 				<td><?php echo $location->name ?></td>
+
+				<?php if ( !$filters->offsetExists('Country') ): ?>
 				<td><?php echo $location->getCountry() ?></td>
+				<?php endif ?>
+				
+				<?php if ( !$filters->offsetExists('Region') ): ?>
 				<td><?php echo $location->getRegion() ?></td>
+				<?php endif ?>
+				
+				<?php if ( !$filters->offsetExists('Island') ): ?>
 				<td><?php echo $location->getIsland() ?></td>
+				<?php endif ?>
+				
 				<td class="report_object_count"><?php echo $location->getNbSamples() ?></td>
 			</tr>
 			<?php endforeach; ?>
