ddd32cf03cd328d361fc37c599137ab0c539964f
Eliezer Talon <elitalon@inventiaplus.com>
2012-01-23 13:42:19 +0000

    Warning message when requesting a full list made optional fixes #1314

--- apps/frontend/templates/_pagination_info.php
+++ apps/frontend/templates/_pagination_info.php
@@ -37,7 +37,11 @@
 		<?php echo link_to(image_tag('next.png', array('alt' => 'Next page', 'title' => 'Next page')), $url.$pager->getNextPage(), array('class' => 'button')) ?>
 		<?php echo link_to(image_tag('last.png', array('alt' => 'Last page', 'title' => 'Last page')), $url.$pager->getLastPage(), array('class' => 'button')) ?>
 	</span>
+	
+	<?php if ( !isset($warning) ) $warning = true ?>
 	<span id="pagination_cancelation">
-		<?php echo link_to('Show all records', "@module_full_index?module=$model", array('confirm' => 'Displaying all results may take some time depending on the number of results. Do you want to continue?')) ?>
+		<?php echo link_to('Show all records',
+			"@module_full_index?module=$model",
+			array('confirm' => ($warning == true)?'Displaying all results may take some time depending on the number of results. Do you want to continue?':false)) ?>
 	</span>
 </div>
