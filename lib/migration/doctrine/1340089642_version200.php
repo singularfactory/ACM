26be1545a4681f64dd3fdf984671827f5c202ad7
Eliezer Talon <elitalon@inventiaplus.com>
2012-06-19 13:57:06 +0100

    Fixed migrations

--- lib/migration/doctrine/1340089642_version200.php
+++ lib/migration/doctrine/1340089642_version200.php
@@ -11,27 +11,17 @@ class Version200 extends Doctrine_Migration_Base {
 			'foreign' => 'id',
 			'foreignTable' => 'petitioners',
 		));
-			 /* $this->addIndex('project', 'project_petitioner_id', array(
-						 'fields' => 
-						 array(
-							0 => 'petitioner_id',
-						 ),
-			 ));*/
+		//$this->addIndex('project', 'project_petitioner_id', array('fields' => array( 0 => 'petitioner_id',),));
 	}
 
 	public function down() {
+		$this->dropForeignKey('project', 'project_petitioner_id_petitioners_id');
 		$this->createForeignKey('project', 'project_petitioner_id_sf_guard_user_id', array(
 			'name' => 'project_petitioner_id_sf_guard_user_id',
 			'local' => 'petitioner_id',
 			'foreign' => 'id',
 			'foreignTable' => 'sf_guard_user',
 		));
-		$this->dropForeignKey('project', 'project_petitioner_id_petitioners_id');
-			 /* $this->removeIndex('project', 'project_petitioner_id', array(
-						 'fields' => 
-						 array(
-							0 => 'petitioner_id',
-						 ),
-			 ));*/
+		//$this->removeIndex('project', 'project_petitioner_id', array('fields' => array( 0 => 'petitioner_id',),));
 	}
 }
