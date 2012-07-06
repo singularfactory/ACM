1337ef921a1c8f5bdc38310e8a314e6fec54c73b
Eliezer Talon <elitalon@inventiaplus.com>
2012-07-06 09:10:10 +0100

    Fixed conditions to create projects

--- apps/frontend/modules/project/templates/newSuccess.php
+++ apps/frontend/modules/project/templates/newSuccess.php
@@ -29,7 +29,7 @@
 
 <?php slot('main_header', 'Add a new project transference') ?>
 <?php
-if ( !$hasStrains || !$hasSamples || !$hasExternalStrains )
+if ( !$hasStrains && !$hasSamples && !$hasExternalStrains )
 	echo '<p>You must first '.link_to('add a strain', 'strain/new').', '.link_to('add a sample', 'sample/new').' or '.link_to('choose a strain from research collection', 'external_strain/index').' before creating projects.</p>';
 elseif( !$hasProjectNames ) {
 	echo '<p>You must '.link_to('add a project name', link_to_backend('project_name_new')).' first before creating projects.</p>';
