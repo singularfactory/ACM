b88be06859632e2e1b8aaff06f1a093cfee4db01
Eliezer Talon <elitalon@gmail.com>
2012-12-11 14:49:27 +0000

    Added arrival date to DNA Lab index

--- apps/frontend/modules/dna_extraction/templates/indexSuccess.php
+++ apps/frontend/modules/dna_extraction/templates/indexSuccess.php
@@ -48,6 +48,7 @@
 			<th><?php echo link_to('Class', '@dna_extraction?sort_column=Strain.TaxonomicClass.name&sort_direction='.$sortDirection.$allResults) ?></th>
 			<th><?php echo link_to('Name', '@dna_extraction?sort_column=Strain.Genus.name&sort_direction='.$sortDirection.$allResults) ?></th>
 			<th class="date extraction_date"><?php echo link_to('Extraction date', '@dna_extraction?sort_column=extraction_date&sort_direction='.$sortDirection.$allResults) ?></th>
+			<th class="date extraction_date"><?php echo link_to('Arrival date', '@dna_extraction?sort_column=arrival_date&sort_direction='.$sortDirection.$allResults) ?></th>
 			<th><?php echo link_to('Extraction kit', '@dna_extraction?sort_column=ExtractionKit.name&sort_direction='.$sortDirection.$allResults) ?></th>
 			<th><?php echo link_to('Concentration ('.sfConfig::get('app_concentration_unit').')', '@dna_extraction?sort_column=concentration&sort_direction='.$sortDirection.$allResults) ?></th>
 			<th class="dna_availability"><?php echo link_to('DNA bank', '@dna_extraction?sort_column=aliquots&sort_direction='.$sortDirection.$allResults)?></th>
@@ -67,6 +68,18 @@
 				<?php
 					if ( $date = $dnaExtraction->getExtractionDate() ) {
 						 $date = format_date($dnaExtraction->getExtractionDate(), 'p');
+					}
+					else {
+						$date = sfConfig::get('app_no_data_message');
+					}
+
+					echo link_to($date, $url);
+				?>
+			</td>
+			<td class="date extraction_date">
+				<?php
+					if ( $date = $dnaExtraction->getArrivalDate() ) {
+						 $date = format_date($dnaExtraction->getArrivalDate(), 'p');
 					}
 					else {
 						$date = sfConfig::get('app_no_data_message');
